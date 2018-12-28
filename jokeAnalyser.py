4b8c78e0b1f7aaf374d87df35e811688eb085660
lev-barbalat <46008106+lev-barbalat@users.noreply.github.com>
2018-12-28 22:49:44 +0200

    Add files via upload

--- jokeAnalyser.py
+++ jokeAnalyser.py
@@ -1,8 +1,41 @@
+CLASSIFICATION_RULES = [
+    ("Одесса", 'Одесса'),
+    ("Одесса", 'Одессит'),
+    ("Одесса", 'одесит'),
+    ("Одесса", 'Одесситка'),
+    ("Одесса", 'Дерибасовская'),
+    ("Одесса", 'Дюк'),
+    ("Одесса", 'Привоз'),
+    ("Компьютер", 'Компьютер'),
+    ("Компьютер", 'компьютер'),
+    ("Программист", 'Программист'),
+    ("Программист", 'программист'),
+    ("Программист", 'програмимирование'),
+    ("Программист", 'компилятор'),
+    ("Евреи", 'Еврей'),
+    ("Евреи", 'раввин'),
+    ("Дети", 'ребенок'),
+    ("Дети", 'дети'),
+
+    ("Евреи", 'Рабинович'),
+    ("Политика", 'Медведев'),
+    ("Политика", 'Путин'),
+    ("Политика", 'Путина'),
+    ("Политика", 'Медеведева'),
+    ("Политика", 'Путину'),
+    ("Политика", 'Медеведеву')
+]
+
+JOKE = "Одесса. Еврейского мальчика лет шести приводят в школу на собеседование"
+JOKE2 = " в школу на собеседование"
+
 class JokeAnalyser:
     textOfJoke=""
+    classifications=set()
 
     def __init__(self,text):
         self.textOfJoke=text
+        self.classifications.clear()
 
     def lenght_of_joke(self):
         return len(self.textOfJoke)
@@ -10,3 +43,24 @@ class JokeAnalyser:
     def number_of_words(self):
         return len(self.textOfJoke.split())
 
+    def classify_joke(self):
+        for rule in CLASSIFICATION_RULES:
+            if (rule[1] in self.textOfJoke):
+                self.classifications.add(rule[0])
+                #print("joke: {0} rule: {1}".format(rule[1], self.textOfJoke))
+
+    def get_classification(self):
+        if len(self.classifications)>0:
+            return ", ".join(self.classifications)
+        else:
+            return ""
+
+
+"""
+joke_analyser=JokeAnalyser(JOKE)
+joke_analyser.classify_joke()
+print(joke_analyser.get_classification())
+joke_analyser2=JokeAnalyser(JOKE2)
+joke_analyser2.classify_joke()
+print(joke_analyser2.get_classification())
+"""
--- jokes_handler.py
+++ jokes_handler.py
@@ -38,7 +38,8 @@ class JokesHandler():
     def analyse_jokes(self):
         for joke in self.jokes:
             current_joke=JokeAnalyser(joke[1])
-            self.analysed_jokes.append((joke[0], joke[1], joke[2], current_joke.lenght_of_joke(), current_joke.number_of_words()))
+            current_joke.classify_joke()
+            self.analysed_jokes.append((joke[0], joke[1], joke[2], current_joke.lenght_of_joke(), current_joke.number_of_words(),current_joke.get_classification()))
 
 
 
@@ -52,7 +53,15 @@ class JokesHandler():
             worksheet.set_column('B:B', 100)
             worksheet.set_column('C:C', 20)
 
-            counter = 0
+            # Captions
+            worksheet.write("A1", "ID")
+            worksheet.write("B1", "Joke")
+            worksheet.write("C1", "Date of Joke")
+            worksheet.write("D1", "Lenght of joke")
+            worksheet.write("E1", "Number of words")
+            worksheet.write("F1", "Categories")
+
+            counter = 1
             for joke in self.analysed_jokes:
                 counter += 1
                 location = "".join(["A", str(counter)])
@@ -65,9 +74,12 @@ class JokesHandler():
                 worksheet.write(location, joke[3])
                 location = "".join(["E", str(counter)])
                 worksheet.write(location, joke[4])
+                location = "".join(["F", str(counter)])
+                categories=", ".join(joke[5])
+                worksheet.write(location, str(joke[5]))
 
             workbook.close()
-            print("{0} analysed jokes were written sucessfully to Excel file: {1}".format(len(self.jokes), path))
+            print("{0} analysed jokes were written sucessfully to Excel file: {1}".format(len(self.jokes), self.analysed_path))
         except Exception as e:
             print("Exception occured: {0}".format(e))
 
@@ -76,12 +88,14 @@ class JokesHandler():
 
 path = PATH_TO_JOKES_FILE + JOKES_FILE + JOKES_FILE_EXTENSION_XLSX
 analysed_path = PATH_TO_JOKES_FILE + ANALYSED_JOKES_FILE + JOKES_FILE_EXTENSION_XLSX
+startOfProcess  = datetime.datetime.now()
 handler=JokesHandler()
 handler.set_path_origin(path)
 handler.set_path_analysed(analysed_path)
 handler.load_jokes_from_excel()
 handler.analyse_jokes()
 handler.write_analysed_to_excel()
-
+elapsed_time=(datetime.datetime.now()-startOfProcess).microseconds/1000
+print("Elapsed time: {0} mili seconds".format(elapsed_time))
 
 
